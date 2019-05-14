aebcd0bcc55c3dc0aa46335e126487f26d0e2bb0
vagrant <vagrant@homestead>
2019-05-14 20:47:55 +0000

    Primer commit

---
+++ resources/views/usuarios.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Listado de usuarios</title>
+  </head>
+  <body>
+    <p>prueba</p>
+  </body>
+</html>
